3bf5c4063589a4ff8798dcc2cc1253bef59148bf
Josiane S Oliveira <josiane.oliveira@sagatechbrasil.com.br>
2020-07-07 15:01:41 -0300

    modificando api

--- src/controllers/UplinkController.js
+++ src/controllers/UplinkController.js
@@ -1,9 +1,24 @@
 import db from '../database'
+import { format } from 'date-fns'
 
 class UplinkController {
     async index(req, res) {
         const { id } = req.params;
-        const { rows } = await db.query("select * from device_up where device_name=$1 and received_at <= CURRENT_TIMESTAMP and received_at>= CURRENT_TIMESTAMP - interval '23 hours' ORDER BY received_at ", [id])
+        let { rows } = await db.query("select * from device_up where device_name=$1 and received_at <= CURRENT_TIMESTAMP and received_at>= CURRENT_TIMESTAMP - interval '23 hours' ORDER BY received_at ", [id])
+        
+        rows = rows.map(row => {
+            const dataArray = []
+
+            for(let i = 0; i < 4; i++) {
+                dataArray.push(row.data[i].toString(16))
+            }
+
+            row.received_at = format(row.received_at, 'dd/MM/Y HH:mm:ss')
+            row.data = parseInt(dataArray.join(''), 16)
+
+            return row;
+        })
+
         res.json(rows)
     }
 }
